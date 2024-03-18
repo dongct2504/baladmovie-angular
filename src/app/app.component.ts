45503c81e296be00b5508fd21901076c86909aca
dongct2504 <dongminhcho89@gmail.com>
2024-03-18 11:52:21 +0700

    init: Initialize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.moviesInTheater = [
+      {
+        title: 'Avenger',
+        price: 200_000,
+        releaseDate: new Date('2012-03-25')
+      },
+      {
+        title: 'Avenger 2',
+        price: 200_000,
+        releaseDate: new Date('2012-03-08')
+      }
+    ];
+
+    // this.moviesFutureRelease = undefined;
+    this.moviesFutureRelease = [
+      {
+        title: 'Avenger 3',
+        price: 200_000,
+        releaseDate: new Date('2012-03-03')
+      },
+      {
+        title: 'Avenger 4',
+        price: 200_000,
+        releaseDate: new Date('2012-03-26')
+      }
+    ];
+  }
+
+  moviesInTheater: any[] | undefined;
+  moviesFutureRelease: any[] | undefined;
+
   title = 'balad-movie';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieListComponent } from './movies/movie-list/movie-list.component';
+import { GenericListComponent } from './utils/generic-list/generic-list.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieListComponent,
+    GenericListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
